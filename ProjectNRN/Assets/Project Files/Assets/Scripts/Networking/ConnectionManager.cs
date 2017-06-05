c92f1900a5f7e77b3d73188e3d6ca1b09b99af41
Reminault <nilemc12@hotmail.com>
2017-06-05 13:09:28 +0400

    Base Files

---
+++ ProjectNRN/Assets/Project Files/Assets/Scripts/Networking/ConnectionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+
+
+namespace NRN.Tales
+{
+	
+	public class ConnectionManager : MonoBehaviour 
+	{
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
