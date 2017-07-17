42f1762c7a4c9306ca0d5dae4e4c47a914b9473b
nabeeha134 <nabeehakhan134@gmail.com>
2017-07-17 15:53:18 +0400

    Player movement based on dice

---
+++ PlayerTileMoves.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTileMoves : MonoBehaviour {
+
+    public List<GameObject> pathTile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
