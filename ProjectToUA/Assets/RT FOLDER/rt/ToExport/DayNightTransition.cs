3cfa2e38b70bf7777289a74660c2a097d5164772
Reminault <nilemc12@hotmail.com>
2017-07-25 11:57:24 +0400

    Canvas and Skybox Sync

---
+++ ProjectToUA/Assets/RT FOLDER/rt/ToExport/DayNightTransition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightTransition : MonoBehaviour {
+
+	bool isSunset;
+	//bool moonRise;
+	float sunRotation, moonRotation;
+
+	public ParticleSystem pS;
+
+	// Use this for initialization
+	void Start () 
+	{
+		isSunset = true;
+		pS.Stop();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float timer = 10f * Time.deltaTime;
+		transform.RotateAround (Vector3.zero, new Vector3 (1f, 0.5f, 0f), timer);
+		transform.LookAt (Vector3.zero);
+
+		if (gameObject.name == "Sun")
+		{
+			sunRotation = this.gameObject.transform.rotation.eulerAngles.x;
+
+			if (Mathf.Ceil (sunRotation) == 350) 
+			{
+				if (!isSunset) 
+				{
+					var psObj = pS.main;
+
+					Color particleColor = Color.Lerp (new Color (255, 255, 174, 255), new Color (255, 255, 174, 0), 20f);
+					psObj.startColor = particleColor;
+					// TODO: If nothing works, enable the comment below.
+					//pS.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+					isSunset = true;
+				} 
+				else if (isSunset) 
+				{
+					pS.Play ();
+					var psObj = pS.main;
+
+					Color particleColor = Color.Lerp (new Color (255, 255, 174, 0), new Color (255, 255, 174, 255), 10f);
+					psObj.startColor = particleColor;
+					isSunset = false;
+				}
+			}
+
+
+		}
+	}
+
+
+}
